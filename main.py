9d2038638cc7b6d23748eb0e951d407eb78c8096
HaseebTahir22 <126325445+HaseebTahir22@users.noreply.github.com>
2024-01-08 20:03:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 print("Hello World")
 cars = ["BMW", "Toyota", "Volvo"]
 print(cars)
-# a = 10
-# b = 20
-# print(a+b)
+ a = 10
+ b = 20
+ print(a+b)
    #Taking user input
-# x = input("Enter first number: ")
-# y = input("Enter second number: ")
-# print(int(x) + int(y))
+ x = input("Enter first number: ")
+ y = input("Enter second number: ")
+ print(int(x) + int(y))
    # Length of string
-# name = "Haseeb"
-# Len = len(name)
-# print(Len)
-# print(name[0:5])
-# print(name[0:6])
+ name = "Haseeb"
+ Len = len(name)
+ print(Len)
+ print(name[0:5])
+ print(name[0:6])
  # remove special characters
-# str1 = "Haseeb!!!"
-# print(str1.rstrip("!"))
+str1 = "Haseeb!!!"
+print(str1.rstrip("!"))
   # replace name
-# print(str1.replace("Haseeb", "John"))
+print(str1.replace("Haseeb", "John"))
  # split name
-# print(str1.split(" "))
+print(str1.split(" "))
        # If else statement
 a = int(input("Enter your age: "))
 print("Your age is:", a)
